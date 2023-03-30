6e9fcde6ff20f7c03b37ba8d5cb3716f025e6f75
Zé Aroldo <73472945+zeharoldoparente@users.noreply.github.com>
2023-03-30 16:36:11 -0400

    começo do js

---
+++ main.js
@@ -0,0 +1,9 @@
+const form = document.getElementById('contact-form');
+const insertName = document.getElementById('insert-name');
+const insertPhone = document.getElementById('insert-phone');
+
+form.addEventListener('submit',function(e) {
+    e.preventDefault();
+
+    alert()
+});
