b1fd6ec81a5f9c9d22a9e6dcec4ba410f872f720
Zé Aroldo <73472945+zeharoldoparente@users.noreply.github.com>
2023-04-02 11:11:06 -0400

    formulario pronto - faltando validações

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
 const form = document.getElementById('contact-form');
-const insertName = document.getElementById('insert-name');
-const insertPhone = document.getElementById('insert-phone');
+let lines = '';
 
 form.addEventListener('submit',function(e) {
     e.preventDefault();
-
-    alert()
+    
+    CreateProfile();
+    
 });
+
+
+function CreateProfile() {
+    const insertName = document.getElementById('insert-name');
+    const insertPhone = document.getElementById('insert-phone');
+
+    let line = '<tr>';
+    line += `<td>${insertName.value}</td>`;
+    line += `<td>${insertPhone.value}</td>`;
+    line += '</tr>';
+
+    lines += line;
+
+    const bodyTable = document.querySelector('tbody');
+    bodyTable.innerHTML = lines;
+    
+    insertName.value = '';
+    insertPhone.value = '';
+}
